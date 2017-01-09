688ff9e4580d62d9a04a793d0b978aa9c95f4c1f
gdewitt <gdewitt@gsu.edu>
2017-01-09 13:19:23 -0500

    initial sharing of MATLAB automations for Munki deployment

---
+++ munki-import-automations/MathWorks-MATLAB/ReadMe.md
@@ -0,0 +1,75 @@
+MathWorks MATLAB Materials
+----------
+Scripts and notes for deploying MathWorks MATLAB with Munki.
+
+## Challenges to Deployment ##
+MathWorks provides a means for performing a silent installation, serialization, and activation of MATLAB using its custom *InstallForMacOSX.app*.  For this case, it's best for Munki to install the vendor's “installer” app, then run it via postinstall script.
+
+1. **Licensing**: The MATLAB installer expects to be fed a path to an **installer input file** which contains some control information, the file installation key, and the paths to two more files:
+   - a **network license server file**:  this file holds the address to the license server, etc.  Fortunately, this format is rather straightforward, and there are examples and notes online.<sup>2</sup>
+   - an **activation file**:  this file holds information about how to activate the product.  Unfortunately, MathWorks does not document the format for this file (at least not in their Mac downloads), but we found other sources online that do.<sup>2</sup>
+
+2. **Install the Installer Problem**: Creating the *pkginfo* for this item requires knowing about Munki's “copy from dmg” installation method, and a solid understanding of how Munki determines if something is installed.  Uninstallation must also be considered, since *remove_copied_items* makes no sense for an “install the installer” case.<sup>1</sup>
+
+## Deployment Overview ##
+1. The MathWorks MATLAB disk image (dmg) should be copied to the Munki repository without modifications.  Be sure you have downloaded the dmg installer from MathWorks!
+2. We'll programmatically create a *pkginfo* file for the “pkg” (the disk image containing the MathWorks installer).  The *make-pkginfo.py* script serves this purpose.
+   * The *make-pkginfo.py* script will request information about the version and installed path for MathWorks MATLAB.
+      - This information should be obtained by installing MathWorks MATLAB on a test Mac first.
+   * The *make-pkginfo.py* script will request license information and other relevant details.
+   * Some notes about the resulting pkginfo:
+      - The resulting pkginfo will be a “copy from dmg” case with an *installs* array that differs from the *items_to_copy* array.  This *installs* array will be populated with the path to the MATLAB app - the “end state.”  This array **won't** hold any path information for the *items_to_copy*.  Recall that Munki relies on the *installs* array when determining if something has been installed already.<sup>1</sup>
+         - The item copied is the *InstallForMacOSX.app* (the vendor's custom installer).
+      - It will contain a postinstall script that:
+         - writes the **installer input file** to */tmp/matlab_installer_input.txt*
+         - writes the **network license server file** to */tmp/matlab_network.lic*
+         - writes the **activation file** to */tmp/matlab_activation.ini*
+         - calls the *InstallForMacOSX.app* to perform the installation
+         - cleans up
+      - It will contain an uninstall script for removing the installed version of MathWorks MATLAB.  The *uninstall_method* is set to *uninstall_script*.
+
+Before You Begin
+----------
+* Review and modify the *make-pkginfo.py* script as necessary for your environment.  The script is easy to read, and there are some global variables that control names, descriptions, and other product metadata.
+   - This script may need some slight modification after each new MathWorks MATLAB version.
+
+Deployment Method
+----------
+## Install MATLAB on a Test Mac ##
+1. Install MATLAB on a test Mac system with the Munki tools installed.  For the test installation, the application can be installed as a trial.  Thus, simply run the *InstallForMacOSX.app* and complete the process manually.
+2. After installation, have Munki figure out the version number by using *makepkginfo* like this:
+   <pre>makepkginfo -f "/Applications/MATLAB_R2016b.app"</pre>
+   * Look for the bundle version (*CFBundleVersion*).  This is the version number that the *make-pkginfo.py* script will request.
+
+## Add MATLAB to the Repository ##
+1. Copy the MathWorks MATLAB disk image to the munki repository.  Use a good naming convention; for example: *MathWorks_MATLAB-version.dmg*, where *version* is the *CFBundleVersion* number determined when you installed it on a test Mac.
+2. Run the *make-pkginfo.py* script and follow its prompts to generate the *pkginfo* file.  For example:
+   <pre>./make-pkginfo.py</pre>
+   * The script is interactive.  It will ask for:
+      - The base path to the Munki repo, if that is not set as a shell environment variable
+      - The MATLAB version as determined by installing it on the test system.  This is where knowing the *CFBundleVersion* is handy.
+      - The path to the installed MATLAB app, relative to the client (example: **/Applications/MATLAB_R2016b.app**)
+      - The path to the MATLAB disk image relative to the Munki repository's *pkgs* directory
+      - The MATLAB file installation key
+      - The license file string
+3. Update catalogs.  Add the software to the appropriate manifest(s) if it is not an “update_for”; for *example*:
+   <pre>makecatalogs</pre>
+   <pre>manifestutil --add-pkg MathWorks_MATLAB --section optional_installs --manifest some_manifest</pre>
+
+Author
+----------
+Written by Gerrit DeWitt (gdewitt@gsu.edu)
+Copyright Georgia State University
+
+Sources
+----------
+1. Munki
+   - https://github.com/munki/munki/wiki/Pkginfo-Files
+   - https://github.com/munki/munki/wiki/Supported-Pkginfo-Keys
+   - https://github.com/munki/munki/wiki/How-Munki-Decides-What-Needs-To-Be-Installed
+2. MATLAB & Silent Installation with Activation
+   - Example MATLAB Munki Import: https://github.com/grahampugh/osx-scripts/tree/master/matlab-munki
+   - MathWorks (Quite Incomplete): Install Noninteractively (Silent Installation): http://www.mathworks.com/help/install/ug/install-noninteractively-silent-installation.html
+   - http://www.mathworks.com/matlabcentral/answers/uploaded_files/43872/activate.txt
+   - http://maithegeek.blogspot.com/2015/10/instsall-matlab-on-linux.html
+   - https://github.com/purpleidea/puppet-matlab/blob/master/templates/installer_input.txt.erb
---
+++ munki-import-automations/MathWorks-MATLAB/make-pkginfo.py
@@ -0,0 +1,237 @@
+#!/usr/bin/env python
+
+# make-pkginfo.py
+# Script for generating pkginfo for deploying MathWorks MATLAB via Munki.
+# Documentation & References: See closest ReadMe.
+
+# Written by Gerrit DeWitt (gdewitt@gsu.edu)
+# 2015-08-24/28, 2015-09-11 (config profile and pkginfo creation), 2015-09-15, 2015-09-30 (first pass at Matlab postinstall scripts)
+# 2015-11-24 (conditions), 2015-11-25, 2015-12-07, 2016-03-21 (EndNote), 2016-07-28, 2016-08-30, 2016-10-03, 2016-11-09, 2016-11-14
+# 2016-11-29 (Autodesk), 2016-12-07, 2016-12-09, 2016-12-12 (SPSS), 2017-01-05/09.
+# Copyright Georgia State University.
+# This script uses publicly-documented methods known to those skilled in the art.
+
+import os, xml, plistlib, subprocess, sys
+
+# Script templates:
+# These are likely to require adjustment for each major MATLAB version.
+global ITEM_MUNKI_POSTINSTALL_SCRIPT_CONTENT_TEMPLATE
+ITEM_MUNKI_POSTINSTALL_SCRIPT_CONTENT_TEMPLATE = '''#!/bin/bash
+installer_bin="/private/tmp/InstallForMacOSX.app/Contents/MacOS/InstallForMacOSX"
+
+# Files used by installer:
+installer_input_file_path="/private/tmp/matlab_installer_input.txt"
+installer_output_file_path="/private/tmp/matlab_installer_output.txt"
+network_license_path="/private/tmp/matlab_network.lic"
+activation_file_path="/private/tmp/matlab_activation.ini"
+
+# File contents:
+installer_input_content="fileInstallationKey=__%app_file_installation_key%__
+agreeToLicense=yes
+outputFile=$installer_output_file_path
+mode=silent
+activationPropertiesFile=$activation_file_path
+licensePath=$network_license_path
+"
+
+network_license_content="__%app_license_string%__
+USE_SERVER
+"
+
+activation_ini_content="isSilent=True
+activateCommand=activateOffline
+licensePath=$network_license_path
+licenseFile=$network_license_path
+"
+
+# Write files used by installer:
+/bin/echo "$installer_input_content" > "$installer_input_file_path"
+/bin/echo "$network_license_content" > "$network_license_path"
+/bin/echo "$activation_ini_content" > "$activation_file_path"
+
+# Call vendor installer:
+"$installer_bin" -inputFile "$installer_input_file_path"
+
+# Clean up:
+if [ -f "$installer_bin" ]; then
+    /bin/rm "$installer_bin"
+fi
+if [ -f "$installer_input_file_path" ]; then
+    /bin/rm "$installer_input_file_path"
+fi
+if [ -f "$network_license_path" ]; then
+    /bin/rm "$network_license_path"
+fi
+if [ -f "$activation_file_path" ]; then
+    /bin/rm "$activation_file_path"
+fi
+'''
+global ITEM_MUNKI_UNINSTALL_SCRIPT_CONTENT_TEMPLATE
+ITEM_MUNKI_UNINSTALL_SCRIPT_CONTENT_TEMPLATE = '''#!/bin/bash
+app_path="__%app_munki_installs_path%__"
+if [ -d "$app_path" ]; then
+    /bin/rm -fr "$app_path"
+fi
+'''
+
+global ITEMS_TO_COPY
+ITEMS_TO_COPY = []
+COPY_ITEM = {'destination_path':"/private/tmp",
+'source_item':"replaced by create_pkginfo()",
+'destination_item':"InstallForMacOSX.app"}
+ITEMS_TO_COPY.append(COPY_ITEM)
+
+global ITEM_MUNKI_INSTALLS_ARRAY
+ITEM_MUNKI_INSTALLS_ARRAY = []
+ITEM_MUNKI_INSTALLS_ITEM = {}
+ITEM_MUNKI_INSTALLS_ITEM['type'] = "application"
+ITEM_MUNKI_INSTALLS_ITEM['path'] = "replaced by create_pkginfo()"
+ITEM_MUNKI_INSTALLS_ITEM['CFBundleVersion'] = "replaced by create_pkginfo()"
+ITEM_MUNKI_INSTALLS_ITEM['version_comparison_key'] = "CFBundleVersion"
+ITEM_MUNKI_INSTALLS_ARRAY.append(ITEM_MUNKI_INSTALLS_ITEM)
+
+def create_items_to_copy_cmds():
+    '''Converts an array of dicts describing items to copy into
+    an array of args for makepkginfo.'''
+    args = []
+    for i_dict in ITEMS_TO_COPY:
+        try:
+            src_item_str = '--itemname=%s' % i_dict['source_item']
+        except KeyError:
+            src_item_str = ''
+        try:
+            dest_item_str = '--destinationitem=%s' % i_dict['destination_item']
+        except KeyError:
+            dest_item_str = ''
+        try:
+            dest_path_str = '--destinationpath=%s' % i_dict['destination_path']
+        except KeyError:
+            src_item_str = ''
+        if dest_path_str and dest_item_str and dest_path_str:
+            args.append(src_item_str)
+            args.append(dest_item_str)
+            args.append(dest_path_str)
+    return args
+
+def create_pkginfo(given_app_version,given_app_munki_installs_path,given_repo_path_to_pkg,given_app_file_installation_key,given_app_license_string):
+    '''Makes the pkginfo for this item.'''
+    # Paths:
+    munki_repo_pkg_path = os.path.join(MUNKI_PKGS_PATH,given_repo_path_to_pkg)
+    munki_repo_pkginfo_path = os.path.join(MUNKI_PKGSINFO_PATH,"%(name)s-%(vers)s" % {"name": ITEM_MUNKI_NAME,"vers":given_app_version})
+    print "Pkginfo will be %s." % munki_repo_pkginfo_path
+    # Postinstall script content:
+    postinstall_script_content = ITEM_MUNKI_POSTINSTALL_SCRIPT_CONTENT_TEMPLATE.replace("__%app_license_string%__",given_app_license_string).replace("__%app_file_installation_key%__",given_app_file_installation_key)
+    # Uninstall script content:
+    uninstall_script_content = ITEM_MUNKI_UNINSTALL_SCRIPT_CONTENT_TEMPLATE.replace("__%app_munki_installs_path%__",given_app_munki_installs_path)
+    # Call makepkginfo:
+    cmd = ['/usr/local/munki/makepkginfo',
+           '--name=%s' % ITEM_MUNKI_NAME,
+           '--catalog=%s' % ITEM_MUNKI_CATALOG_NAME,
+           '--description=%s' % ITEM_MUNKI_DESCRIPTION,
+           '--developer=%s' % ITEM_MUNKI_DEVELOPER_NAME,
+           '--category=%s' % ITEM_MUNKI_CATEGORY,
+           '--unattended_install',
+           '--pkgvers=%s' % given_app_version,
+           '--displayname=%s' % ITEM_MUNKI_DISP_NAME]
+    # Update source item for "items to copy":
+    ITEMS_TO_COPY[0]['source_item'] = ITEMS_TO_COPY_SRC_ITEM
+    # Make "items to copy" arg string:
+    itc_args = create_items_to_copy_cmds()
+    if not itc_args:
+        return False
+    cmd.extend(itc_args)
+    cmd.append(munki_repo_pkg_path)
+    try:
+        output = subprocess.check_output(cmd)
+    except subprocess.CalledProcessError:
+        print "Error creating pkginfo!"
+        return False
+    try:
+        output_dict = plistlib.readPlistFromString(output)
+    except xml.parsers.expat.ExpatError:
+        return False
+    # Other keys if defined:
+    try:
+        output_dict['minimum_os_version'] = ITEM_MUNKI_MIN_OS
+    except NameError:
+        pass
+    try:
+        output_dict['postinstall_script'] = postinstall_script_content
+    except NameError:
+        pass
+    try:
+        output_dict['installs'] = ITEM_MUNKI_INSTALLS_ARRAY
+    except NameError:
+        pass
+    try:
+        output_dict['installs'][0]['CFBundleVersion'] = given_app_version
+        output_dict['installs'][0]['path'] = given_app_munki_installs_path
+    except KeyError, IndexError:
+        pass
+    # Uninstall method:
+    output_dict['uninstall_method'] = "uninstall_script"
+    try:
+        output_dict['uninstall_script'] = uninstall_script_content
+    except NameError:
+        pass
+
+    plistlib.writePlist(output_dict,munki_repo_pkginfo_path)
+    return True
+
+def main():
+    '''Main logic.'''
+    # Gather Munki repository info:
+    global MUNKI_REPO_PATH, MUNKI_PKGS_PATH, MUNKI_PKGSINFO_PATH
+    try:
+        MUNKI_REPO_PATH = os.environ['MUNKI_REPO_PATH']
+    except KeyError:
+        MUNKI_REPO_PATH = raw_input("Munki repo base path: ")
+    if not os.path.exists(MUNKI_REPO_PATH):
+        print "Munki repository does not exist at %s!" % MUNKI_REPO_PATH
+        sys.exit(1)
+    MUNKI_PKGS_PATH = os.path.join(MUNKI_REPO_PATH,'pkgs')
+    MUNKI_PKGSINFO_PATH = os.path.join(MUNKI_REPO_PATH,'pkgsinfo')
+
+    # General variables; edit to suit your needs:
+    global ITEM_MUNKI_NAME,ITEM_MUNKI_DISP_NAME,ITEM_MUNKI_MIN_OS, ITEM_MUNKI_CATALOG_NAME, ITEM_MUNKI_DESCRIPTION, ITEM_MUNKI_DEVELOPER_NAME, ITEM_MUNKI_CATEGORY, ITEM_MUNKI_POSTINSTALL_SCRIPT_CONTENT_TEMPLATE
+    ITEM_MUNKI_NAME = "MathWorks_MATLAB"
+    ITEM_MUNKI_DISP_NAME = "MathWorks MATLAB"
+    ITEM_MUNKI_MIN_OS = "10.10.5"
+    ITEM_MUNKI_CATALOG_NAME = "software"
+    ITEM_MUNKI_DEVELOPER_NAME = "MathWorks"
+    ITEM_MUNKI_CATEGORY = "Math & Statistics"
+    ITEM_MUNKI_DESCRIPTION = "Installs MathWorks MATLAB configured to use the license server."
+    global ITEMS_TO_COPY_SRC_ITEM
+    ITEMS_TO_COPY_SRC_ITEM = "InstallForMacOSX.app"
+
+    # Gather item info:
+    app_version = raw_input("MATLAB version: ")
+    app_munki_installs_path = raw_input("Path where app is installed (relative to client, should start with /Applications): ")
+    repo_path_to_pkg = raw_input("Path to the item in repo (relative to %s): " % MUNKI_PKGS_PATH)
+    print '''
+MATLAB File Installation Key:
+   - This is a string of 12 groups of 5 numbers separated by dashes.
+   - Example: 11111-22222-33333-44444-55555-66666-77777-88888-99999-00000-11111-22222
+'''
+    app_file_installation_key = raw_input("MATLAB File Installation Key: ")
+    print '''
+License File String:
+   - This includes the host name of the license server with some extra details.
+   - Example: SERVER server.host.name AABB11223344 27000
+'''
+    app_license_string = raw_input("License File String: ")
+    print '''
+Is this version an update for another version of MATLAB?
+   - If not a patch, simply press return.
+   - If a patch, provide the name key of the parent MATLAB it patches.
+'''
+
+    # Generate pkginfo:
+    if not create_pkginfo(app_version,app_munki_installs_path,repo_path_to_pkg,app_file_installation_key,app_license_string):
+        print "Failed to create pkginfo."
+        sys.exit(1)
+    # All OK if here:
+    sys.exit(0)
+
+if __name__ == "__main__":
+    main()
