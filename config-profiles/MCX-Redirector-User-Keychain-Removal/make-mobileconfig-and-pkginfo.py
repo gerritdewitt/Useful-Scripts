89a1667407040770af21f01c8dc683d5fb4cae7f
Gerrit DeWitt <gdewitt@gsu.edu>
2017-05-26 16:20:52 -0400

    missing import sys; removing import subprocess

--- config-profiles/MCX-Redirector-User-Keychain-Removal/make-mobileconfig-and-pkginfo.py
+++ config-profiles/MCX-Redirector-User-Keychain-Removal/make-mobileconfig-and-pkginfo.py
@@ -16,7 +16,7 @@
 # Copyright Georgia State University.
 # This script uses publicly-documented methods known to those skilled in the art.
 
-import os, uuid, plistlib, subprocess
+import os, uuid, plistlib, sys
 
 def make_mobileconfig():
     '''Generates a configuration profile with MCX, etc.'''
